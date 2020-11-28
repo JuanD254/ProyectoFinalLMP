0284b47f61ea5427d21525ccab806aba27b41c43
JuanD254 <juan.daniel.moron@gmail.com>
2020-11-28 15:34:30 -0600

    Librerías
    Agregue librerías que usaré luego

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,7 @@
-print("Proyecto Final Lenguajes Modernos de Programación")
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String,Integer,Column,DateTime
+from datetime import datetime
+
+print("Número de contagiados en el Estado de Nuevo León")
